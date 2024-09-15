15b36980a7dff1217c5cbb59eb552bba7db925b1
Swelen <swelens2@hotmail.com>
2024-09-15 00:08:54 -0300

    feature: create User model, set up repository, and implement calls to endpoint

---
+++ GithubUsers/GithubUsers/User/Repository/ResponseError.swift
@@ -0,0 +1,12 @@
+//
+//  Error.swift
+//  GithubUsers
+//
+//  Created by Swelen Ebert on 14/09/24.
+//
+
+import Foundation
+
+enum ResponseError: Error {
+    case notFound
+}
--- GithubUsers/GithubUsers/User/Repository/UserRepository.swift
+++ GithubUsers/GithubUsers/User/Repository/UserRepository.swift
@@ -5,4 +5,74 @@
 //  Created by Swelen Ebert on 13/09/24.
 //
 
-import Foundation
+import Alamofire
+
+protocol UserRepositoryProtocol: AnyObject {
+    func getAllUsers(completion: @escaping ([User]?, Error?) -> ())
+    func getUser(user: String, completion: @escaping (User?, Error?) -> ())
+    func getUserRepos(user: String, completion: @escaping ([Repo]?, Error?) -> ())
+}
+
+class UserRepository: UserRepositoryProtocol {
+    func getAllUsers(completion: @escaping ([User]?, Error?) -> ()) {
+        AF.request(Environment.BASE_URL, method: .get, parameters: nil, encoding: JSONEncoding.default).responseData { (response) in
+            switch response.result {
+                
+            case .success:
+                guard let data = response.data else { return }
+                do {
+                    let result = try JSONDecoder().decode ([User].self, from: data)
+                    completion(result, nil)
+                } catch {
+                    completion([], response.error)
+                }
+            case .failure(_):
+                completion([], response.error)
+            }
+        }
+    }
+    
+    func getUser(user: String, completion: @escaping (User?, Error?) -> ()) {
+        AF.request(Environment.BASE_URL + "/" + user, method: .get, parameters: nil, encoding: JSONEncoding.default).responseData { (response) in
+            guard let statusCode = response.response?.statusCode else { return }
+            if statusCode == 404 {
+                let error = ResponseError.notFound
+                completion(nil, error)
+                return
+            }
+            
+            switch response.result {
+            case .success:
+                guard let data = response.data else { return }
+                do {
+                    let result = try JSONDecoder().decode (User.self, from: data)
+                    completion(result, nil)
+                } catch {
+                    completion(nil, response.error)
+                }
+            case .failure(let error):
+                completion(nil, error)
+            }
+        }
+    }
+    
+    func getUserRepos(user: String, completion: @escaping ([Repo]?, Error?) -> ()) {
+        AF.request(Environment.BASE_URL + "/" + user + Environment.SUFIXO, method: .get, parameters: nil, encoding: JSONEncoding.default).responseData { (response) in
+            switch response.result {
+                
+            case .success:
+                guard let data = response.data else { return }
+                do {
+                    let result = try JSONDecoder().decode ([Repo].self, from: data)
+                    completion(result, nil)
+                } catch {
+                    completion([], response.error)
+                }
+            case .failure(_):
+                completion([], response.error)
+            }
+        }
+    }
+    
+    
+}
